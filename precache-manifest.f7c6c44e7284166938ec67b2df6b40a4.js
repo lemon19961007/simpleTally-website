4ccaeb26f5f3ba4c41d07db04c81416427b143f5
sherry <lemon19961007@sina.com>
2022-09-14 12:08:55 +0800

    deploy

---
+++ precache-manifest.f7c6c44e7284166938ec67b2df6b40a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03c452a7d7ff6f85ed44",
+    "url": "/simpleTally-website/css/app.57284c97.css"
+  },
+  {
+    "revision": "12102e27d149b6ccde413d40677d4df7",
+    "url": "/simpleTally-website/index.html"
+  },
+  {
+    "revision": "03c452a7d7ff6f85ed44",
+    "url": "/simpleTally-website/js/app.78aef119.js"
+  },
+  {
+    "revision": "80842000eab6aea9d21b",
+    "url": "/simpleTally-website/js/chunk-vendors.02697a75.js"
+  },
+  {
+    "revision": "3b3ac03b730001df064d3e92befbe4e5",
+    "url": "/simpleTally-website/manifest.json"
+  }
+]);
